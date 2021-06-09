1a29a849396a9b16d13f0450d71b626ff3eb93fb
TempoQuill <jicrapo@gmail.com>
2021-06-08 21:48:06 -0400

    Add some pokemon build data

---
+++ data/pokemon/assembly.asm
@@ -0,0 +1,12 @@
+item_pokecombo: MACRO
+	dw \1 ; species
+rept (_NARG - 1) / 2
+	db \2, \3 ; item, quantity
+	shift 2
+endr
+	ds 9 - ((_NARG - 1) / 2)
+ENDM
+
+ItemPokemonCombinations:
+	item_pokecombo MAGNEMITE, MAGNET,     3, SILVER_PEARL, 1
+	item_pokecombo STARYU,    STAR_PIECE, 6, PEARL,        1, STARDUST, 1
