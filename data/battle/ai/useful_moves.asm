a9188dc1c72d9dd8d16e720a4e7fe3dab9e398d9
TempoQuill <jicrapo@gmail.com>
2022-05-26 09:42:59 -0400

    Describe moves, remove hyper beam

--- data/battle/ai/useful_moves.asm
+++ data/battle/ai/useful_moves.asm
@@ -1,23 +1,22 @@
 ; AI_SMART knows these moves are usable all-around.
 
 UsefulMoves:
-	db DOUBLE_EDGE
-	db SING
-	db FLAMETHROWER
-	db HYDRO_PUMP
-	db SURF
-	db ICE_BEAM
-	db BLIZZARD
-	db HYPER_BEAM
-	db SLEEP_POWDER
-	db THUNDERBOLT
-	db THUNDER
-	db EARTHQUAKE
-	db TOXIC
-	db PSYCHIC_M
-	db HYPNOSIS
-	db RECOVER
-	db FIRE_BLAST
-	db SOFTBOILED
-	db SUPER_FANG
+	db DOUBLE_EDGE  ; +0 heavy damage, recoil
+	db SING         ; +0 sleep move, inaccurate
+	db FLAMETHROWER ; +2 can burn, heavy damage
+	db HYDRO_PUMP   ; +1 very heavy damage, inaccurate
+	db SURF         ; +1 heavy damage
+	db ICE_BEAM     ; +1 can freeze
+	db BLIZZARD     ; +1 can freeze, heavy damage, inaccurate
+	db SLEEP_POWDER ; +1 sleep move
+	db THUNDERBOLT  ; +2 can paralyze, heavy damage
+	db THUNDER      ; +2 can paralyze, very heavy damage, inaccurate
+	db EARTHQUAKE   ; +1 heavy damage
+	db TOXIC        ; +2 heavy poison move
+	db PSYCHIC_M    ; +3 can lower special, heavy damage
+	db HYPNOSIS     ; +0 sleep move, inaccurate
+	db RECOVER      ; +1 healing move
+	db FIRE_BLAST   ; +2 can burn, very heavy damage, inaccurate
+	db SOFTBOILED   ; +1 healing move
+	db SUPER_FANG   ; +0 halves enemy health
 	db -1 ; end
