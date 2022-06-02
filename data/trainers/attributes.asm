44a6f4124f3059a9997b31d367351f1106cbe39c
TempoQuill <jicrapo@gmail.com>
2022-06-02 18:27:30 -0400

    Buff base reward

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -3,13 +3,13 @@ TrainerClassAttributes:
 
 ; Falkner (leftover), James
 	db POTION, FULL_HEAL ; items
-	db 25 ; base reward
+	db 75 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Whitney (leftover), Cindy
 	db POTION, FULL_HEAL ; items
-	db 25 ; base reward
+	db 75 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
@@ -51,7 +51,7 @@ TrainerClassAttributes:
 
 ; Rival1
 	db NO_ITEM, NO_ITEM ; items
-	db 15 ; base reward
+	db 20 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
@@ -249,7 +249,7 @@ TrainerClassAttributes:
 
 ; Rival2
 	db NO_ITEM, NO_ITEM ; items
-	db 25 ; base reward
+	db 30 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
