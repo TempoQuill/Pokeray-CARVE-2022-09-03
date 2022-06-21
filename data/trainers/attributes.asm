55d665d83f4cb6a39a12c4aed048e95a3d809904
TempoQuill <jicrapo@gmail.com>
2022-06-21 10:47:46 -0400

    Finalize rival's initial party

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -50,7 +50,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Rival1
-	db NO_ITEM, NO_ITEM ; items
+	db POTION, POTION ; items
 	db 20 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
