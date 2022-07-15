831989825aca9b4d5e6cff7eb0039a31046dc5e5
TempoQuill <jicrapo@gmail.com>
2022-07-15 07:46:06 -0400

    Do box dex entries

---
+++ data/pokemon/dex_entries/ray/helicelia.asm
@@ -0,0 +1,10 @@
+	db "SUNLIGHT" ; species name
+	dw 808, 1400 ; height, weight
+
+	db   "It soars EQUINTO"
+	next "during the day to"
+	next "make sure that the"
+
+	page "region is at peace"
+	next "while it roosts on"
+	next "MT. VACUUM.@"
---
+++ data/pokemon/dex_entries/ray/selenumia.asm
@@ -0,0 +1,10 @@
+	db "MOONLIGHT" ; species name
+	dw 807, 1399 ; height, weight
+
+	db   "It soars around"
+	next "EQUINTO at night,"
+	next "looking for its"
+
+	page "own place in #-"
+	next "MON. It likes to"
+	next "prank trainers.@"
