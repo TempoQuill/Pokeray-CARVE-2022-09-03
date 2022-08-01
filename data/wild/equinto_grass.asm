d7884ce574a0b320b4e12e4027bf690378a35f9f
TempoQuill <jicrapo@gmail.com>
2022-08-01 16:03:58 -0400

    Update grassmons

--- data/wild/equinto_grass.asm
+++ data/wild/equinto_grass.asm
@@ -1,28 +1,120 @@
+wild: MACRO
+	db \1 ; level
+	dw \2 ; species
+ENDM
+
+wild_encounter: MACRO
+	db \1 percent, \1 percent, \1 percent ; rates: morn/day/nite
+ENDM
+
 EquintoGrassWildMons:
 
 	map_id ROUTE_49
-	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	wild_encounter 10 ; encounter rate
 	; morning
-	db 2, PICHU
-	db 2, TOGEPI
-	db 2, PIDGEY
-	db 2, NATU
-	db 2, MAREEP
-	db 2, HOPPIP
-	db 5, SPINARAK
+	wild 2, PIDGEY
+	wild 2, SPEAROW
+	wild 2, SENTRET
+	wild 2, LEDYBA
+	wild 5, CLEFFA
+	wild 5, IGGLYBUFF
+	wild 2, ASSISDRA
 	; day
-	db 2, SENTRET
-	db 2, CATERPIE
-	db 2, PIDGEY
-	db 2, SPEAROW
-	db 2, LEDYBA
-	db 2, WEEDLE
-	db 5, SPINARAK
+	wild 2, PIDGEY
+	wild 2, SPEAROW
+	wild 2, RATTATA
+	wild 2, SENTRET
+	wild 2, CATERPIE
+	wild 3, RATTATA
+	wild 5, PICHU
 	; nite
-	db 2, HOOTHOOT
-	db 2, CLEFFA
-	db 2, IGGLYBUFF
-	db 2, ZUBAT
-	db 2, SPINARAK
-	db 2, PICHU
-	db 5, WOOPER
+	wild 2, HOOTHOOT
+	wild 2, CROVEN
+	wild 2, RATTATA
+	wild 5, CLEFFA
+	wild 5, IGGLYBUFF
+	wild 5, PICHU
+	wild 5, COINTEN
+
+	map_id ROUTE 50
+	wild_encounter 10 ; encounter rate
+	; morning
+	wild 3, PIDGEY
+	wild 3, SPEAROW
+	wild 3, SENTRET
+	wild 3, LEDYBA
+	wild 5, CLEFFA
+	wild 5, IGGLYBUFF
+	wild 3, ASSISDRA
+	; day
+	wild 3, PIDGEY
+	wild 3, SPEAROW
+	wild 3, RATTATA
+	wild 3, SENTRET
+	wild 3, CATERPIE
+	wild 4, RATTATA
+	wild 5, PICHU
+	; nite
+	wild 3, HOOTHOOT
+	wild 3, CROVEN
+	wild 3, RATTATA
+	wild 5, CLEFFA
+	wild 5, IGGLYBUFF
+	wild 5, PICHU
+	wild 5, GATOLL
+
+	map_id ROUTE 51
+	wild_encounter 10 ; encounter rate
+	; morning
+	wild 4, PIDGEY
+	wild 4, SPEAROW
+	wild 4, SENTRET
+	wild 4, LEDYBA
+	wild 5, CLEFFA
+	wild 5, IGGLYBUFF
+	wild 4, ASSISDRA
+	; day
+	wild 4, PIDGEY
+	wild 4, SPEAROW
+	wild 4, RATTATA
+	wild 4, SENTRET
+	wild 4, WEEDLE
+	wild 5, RATTATA
+	wild 5, PICHU
+	; nite
+	wild 4, HOOTHOOT
+	wild 4, CROVEN
+	wild 4, RATTATA
+	wild 5, CLEFFA
+	wild 5, IGGLYBUFF
+	wild 5, PICHU
+	wild 5, GATOLL
+
+	map_id MT_VACUUM
+	wild_encounter 10 ; encounter rate
+	; morning
+	wild 4, GEODUDE
+	wild 4, ZUBAT
+	wild 5, SANDSHREW
+	wild 6, EKANS
+	wild 6, DUNSPARCE
+	wild 7, FOSSLIX
+	wild 5, GASTLY
+	; day
+	wild 4, GEODUDE
+	wild 4, ZUBAT
+	wild 5, SANDSHREW
+	wild 6, EKANS
+	wild 6, DUNSPARCE
+	wild 7, FOSSLIX
+	wild 5, GASTLY
+	; nite
+	wild 4, GEODUDE
+	wild 4, ZUBAT
+	wild 5, SANDSHREW
+	wild 6, EKANS
+	wild 6, DUNSPARCE
+	wild 7, FOSSLIX
+	wild 5, GASTLY
+
+	db -1
