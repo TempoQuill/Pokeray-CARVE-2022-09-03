e6bcb027223f156983549db980c3185a25905a49
TempoQuill <jicrapo@gmail.com>
2022-09-04 09:08:32 -0400

    Align music Data

--- audio/music_pointers.asm
+++ audio/music_pointers.asm
@@ -8,12 +8,12 @@ Music:
 	dba Music_TitleScreen
 	dba Music_MainMenu
 	dba Music_HealPokemon
-	dba Keiro29                   ; Music_Route49
+	dba Keiro29                    ; Music_Route49
 	dba Music_Route52
 	dba Music_Route55
 	dba Music_Mom
-	dba Music_ShowMeAround        ; HurryAlong
-	dba Music_RBYTradeFanMade     ; Indoors
+	dba Music_ShowMeAround         ; HurryAlong
+	dba Music_RBYTradeFanMade      ; Indoors
 	dba Music_PokemonCenter
 	dba Music_CottageTown
 	dba Music_TraversalCity
@@ -21,52 +21,52 @@ Music:
 	dba Music_TrainerBattle
 	dba Music_GymBattle
 	dba Music_PartyHostBattle
-	dba Music_FinalBattle         ; ShaggyBattle
-	dba Music_ChampionBattle
-	dba Music_LookOfficer         ; LookWarden
-	dba Music_LookLass            ; LookGirl
-	dba Music_LookHiker           ; LookLunatic
-	dba Music_LookYoungster       ; LookYouth
-	dba Music_LookBeauty          ; LookWoman
+	dba Music_FinalBattle          ; ShaggyBattle
+	dba Music_ChampionBattle       ; ParkerBattle
+	dba Music_LookOfficer          ; LookWarden
+	dba Music_LookLass             ; LookGirl
+	dba Music_LookHiker            ; LookLunatic
+	dba Music_LookYoungster        ; LookYouth
+	dba Music_LookWoman
 	dba Music_LookSibs
 	dba Music_LookPartyHost
 	dba Music_LookArtist
 	dba Music_LookRival
 	dba Music_Larch
 	dba Music_AfterTheRivalFight
-	dba Music_SuccessfulCapture   ; CatchVictory
-	dba Music_WildPokemonVictory  ; WildVictory
+	dba Music_SuccessfulCapture    ; CatchVictory
+	dba Music_WildPokemonVictory   ; WildVictory
 	dba Music_TrainerVictory
-	dba Music_GymLeaderVictory    ; GymVictory
+	dba Music_GymLeaderVictory     ; GymVictory
 	dba Music_Gym
-	dba Music_RGOldHeal           ; HealAlternate
+	dba Music_RGOldHeal            ; HealAlternate
 ; end of demo OST
 	dba Music_GameCorner
 	dba Music_Bicycle
 	dba Music_HallOfFame
 	dba Music_OddishFarm
-	dba Music_Evolution           ; Event
-	dba Music_StaffRoll
+	dba Music_Evolution            ; Event
+	dba Music_CreditsExt
 	dba Music_LanaweForest
-	dba Music_FireFlower          ; SSVapor
-	dba Music_Pokecenter          ; DilapPokeCenter
-	dba Music_Starman             ; Lighthouse
+	dba Music_FireFlower           ; SSVapor
+	dba Music_DilapPokeCenter
+	dba Music_Starman              ; Lighthouse
 	dba Music_EastCoastStop
-	dba Music_IndigoPlateau       ; PokemonLeague
+	dba Music_IndigoPlateau        ; PokemonLeague
 	dba Music_GolemPalace
 	dba Music_HypercoinCity
-	dba Music_DiglettCave         ; MtVacuum
-	dba Music_NorgeoTown
-	dba Music_GoldenrodCity       ; PortiteCity
-	dba End2                      ; TheEnd
+	dba Music_DiglettCave          ; MtVacuum
+	dba Music_AzaleaTown           ; NorgeoTown
+	dba Music_GoldenrodCity        ; PortiteCity
+	dba Music_PostCredits          ; End2 ; Music_TheEnd
 	dba Music_Printer
 	dba Music_Route61
 	dba Music_LookHavoc
-	dba Music_HavocTheme
-	dba Music_RBYSurf             ; SSVaporDeparture
-	dba Music_VSMatchPlayer1      ; StampTown
+	dba Music_TeamHavocTheme
+	dba Music_RBYSurf              ; SSVaporDeparture
+	dba Music_VSMatchPlayer1       ; StampTown
 	dba Music_SunkernSwamp
 	dba Music_Surf
 	dba Music_HavocHideout
-	dba Music_VSMenu1             ; TradeMenuPart1
-	dba Music_VSMenu2             ; TradeMenuPart2
+	dba Music_RBYBike              ; TradeMenuPart1
+	dba Music_BugCatchingConestExt ; TradeMenuPart2
